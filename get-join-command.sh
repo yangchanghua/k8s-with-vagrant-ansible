1413c8a256c03c84f13a0351461c13daaccade8a
ricyang <yangchanghua@gmail.com>
2020-11-13 22:26:28 +0800

    idempotent command for joining cluster

--- get-join-command.sh
+++ get-join-command.sh
@@ -3,8 +3,29 @@ pubkey=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt \
     | openssl dgst -sha256 -hex \
     | sed 's/^.* //')
 
+if [ -z "$pubkey" ]; then
+	echo "No public key exists!"
+	exit 1
+fi
 
-token=$(kubeadm token list | grep 'default boot' | cut -f1 -d' ')
+token=$(kubeadm token list | grep -v TOKEN | grep default-node-token | head -1 | cut -f1 -d' ')
+if [ -z "$token" ]; then
+	echo "No token exists, create it now..."
+	kubeadm token create --print-join-command
+	token=$(kubeadm token list | grep -v TOKEN | grep default-node-token | head -1 | cut -f1 -d' ')
+	if [ -z "$token" ]; then
+		echo "Token creation failed!"
+		exit 1
+	fi
+fi
+
+IP=$(kubectl get nodes -lnode-role.kubernetes.io/master -ojsonpath='{..addresses[?(@.type=="InternalIP")].address}')
+if [ -z "$IP" ]; then
+  echo "Cannot get master ip"
+  exit 1
+fi
+
+PORT=6443
 
 
-echo "kubeadm join --token=$token --discovery-token-ca-cert-hash sh2256:$pubkey"
+echo "kubeadm join $IP:$PORT --token=$token --discovery-token-ca-cert-hash sh2256:$pubkey"
